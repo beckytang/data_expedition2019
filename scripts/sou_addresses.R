fcc863c18389031ba4e1ebc7ccb7a958fe04537d
Becky Tang <btang96@gmail.com>
2019-07-28 20:06:07 -0400

    Forgot to include political party

--- scripts/sou_addresses.R
+++ scripts/sou_addresses.R
@@ -9,18 +9,21 @@
 #############
 setwd("~/data_expedition2019/data/state_of_union")
 require(tidyverse)
+require(readxl)
 
 sou_complete <- data.frame()
 files <- list.files(pattern=".txt")
 for (f in files){
   name <- f
-  year<- substring(name, first = 1, last = 4)
-  prez <- substring(name, first = 5, last = nchar(name)-4)
+  year<- as.numeric(substring(name, first = 1, last = 4))
   temp <- read.table(f, header = F, sep = "\n", quote = "")
   temp <- temp %>% as.data.frame() %>% 
     rename("text" = 1) %>% 
     mutate(text = as.character(text)) %>%
     mutate(paragraph = row_number()) %>%
-    mutate(president = prez, year = year)
+    mutate(Year = year)
   sou_complete <- rbind(sou_complete, temp)
 }
+
+tab <- read_xlsx("stateofunion.xlsx")
+left_join(sou_complete, tab, by = "Year")
